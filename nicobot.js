ccfa19899c655990dad00883e15da84a1a7b63aa
GorkaG <gargoka@hotmail.com>
2018-07-03 15:50:27 +0200

    Create nicobot.js

---
+++ nicobot.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const helpers = require('./helpers.js');
+const bot = new Discord.Client();
+ bot.on('ready', () => {
+     console.log('Nico Nico Nii');
+ });
+
+let prefix = process.env.PREFIX;
+bot.on('message', (message) => {
+  if(message.author.bot) return;
+  helpers.handleMessageDeletions(message);
+  helpers.handleLinkDeletions(message);
+  if (message.content == 'hi') {
+        message.channel.sendMessage('Nico Nico Nii');
+    }
+  if (message.content == 'hola') {
+          message.channel.sendMessage('Nico Nico Nii');
+      }
+  if (message.content == 'hello') {
+            message.channel.sendMessage('Nico Nico Nii');
+        }
+});
+bot.login(process.env.TOKEN);
