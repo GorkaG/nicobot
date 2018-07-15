ef78addb13c4721e8d72b75df67bb99efb0bb4b5
YainmaX <40799968+YainmaX@users.noreply.github.com>
2018-07-15 22:48:38 +0200

    Update giveaway.js

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -21,7 +21,7 @@ function makeFilters(author) {
         },
         reactionsFilter(reaction,user){
             //console.log(user);
-            const returnedValue = reaction.emoji.name === '👍' && !user.bot;
+            const returnedValue = reaction.emoji.name === '<:nico:468155749374361600>' && !user.bot;
             return returnedValue;
         }
     }
@@ -87,7 +87,7 @@ function makeSteps(filter,channel) {
         Prize: ${prize}
             `)
             .then(message => {
-                message.react("👍");
+                message.react("<:nico:468155749374361600>");
                 return message.awaitReactions(currentFilter.reactionsFilter,{time: time*1000});
             })
         },
