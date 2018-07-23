34a7e13d0d1eba58819e7e21d6243c88a0f053a1
GorkaG <gargoka@hotmail.com>
2018-07-23 14:32:04 +0200

    Update adminHelp.js

--- commands/adminHelp.js
+++ commands/adminHelp.js
@@ -1,5 +1,9 @@
 var {prefix} = require('../config.json');
 const {isAdmin} = require('../helpers/admin');
+const NO_ADMIN_EXCEPTION = "noAdminException";
+const NO_CHANNEL_EXCEPTION = "noChannelException";
+const NO_MESSAGE_EXCEPTION = "noMessageException";
+const WAIT_TIME = 600*1000;
 
 module.exports = {
     name: 'adminhelp',
@@ -18,5 +22,44 @@ Description: ${next.description}
         });
         acc += '```';
         message.author.createDM().then(dmChannel => dmChannel.send(acc));
+        const guild = message.guild;
+        const author = message.author;
+        const channels = message.mentions.channels;
+        let channelWillTalk;
+        const isAdminQuery = isAdmin(guild,author);
+        
+        isAdminQuery
+            .then(isAdmin =>{
+                if(!isAdmin){
+                    throw NO_ADMIN_EXCEPTION;
+                }
+            })
+            .then(()=>{
+                if(channels.first() === undefined){
+                    throw NO_CHANNEL_EXCEPTION;
+                }
+                channelWillTalk = channels.first();
+            })
+            .then(() =>{
+                const filter = nextMessage => nextMessage.author.id === message.author.id;
+                message.channel.send("The next message will be written as nico <:NicoNicoNii:360455426745303041> ")
+                return message.channel.awaitMessages(filter,{max:1, time: WAIT_TIME })
+            })
+            .then(collectedMessages => {
+                if(collectedMessages.first() === undefined){
+                    throw NO_MESSAGE_EXCEPTION;
+                }
+                channelWillTalk.send(collectedMessages.first().content);
+            })
+            .catch(error =>{
+                if(error === NO_ADMIN_EXCEPTION){
+                    console.log("someone is trying to invoke admin commands from non admin");
+                } else if(error === NO_CHANNEL_EXCEPTION){
+                    message.channel.send("You must provide a channel where you want to talk");
+                } else if(error === NO_MESSAGE_EXCEPTION){
+                    message.channel.send("You must provide a message to talk");
+                } else {
+                    console.log("UNKNOWN Error on talk",error)
+                }
     },
 };
